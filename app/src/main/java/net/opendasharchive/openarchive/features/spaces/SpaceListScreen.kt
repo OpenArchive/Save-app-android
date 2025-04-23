0e61a0d82a10946c89f3764c0968b23983782332
Elelan's Macbook Pro <evic2132@sysco.com>
2025-04-23 23:57:19 +0530

    fixed issue for server list empty state not showing correctly added SpaceListViewModel

--- app/src/main/java/net/opendasharchive/openarchive/features/spaces/SpaceListScreen.kt
+++ app/src/main/java/net/opendasharchive/openarchive/features/spaces/SpaceListScreen.kt
@@ -1,8 +1,6 @@
 package net.opendasharchive.openarchive.features.spaces
 
-import android.content.res.Configuration
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,43 +8,61 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import net.opendasharchive.openarchive.R
 import net.opendasharchive.openarchive.core.presentation.theme.DefaultScaffoldPreview
 import net.opendasharchive.openarchive.db.Space
 import net.opendasharchive.openarchive.features.main.ui.components.SpaceIcon
 import net.opendasharchive.openarchive.features.main.ui.components.dummySpaceList
-import net.opendasharchive.openarchive.R
+import org.koin.androidx.compose.koinViewModel
+
+
+class SpaceListViewModel() : ViewModel() {
+
+    private val _spaceList = MutableStateFlow<List<Space>>(emptyList())
+    val spaceList: StateFlow<List<Space>> = _spaceList
+
+    fun refreshSpaces() {
+        _spaceList.value = Space.getAll().asSequence().toList()
+    }
+}
 
 @Composable
 fun SpaceListScreen(
     onSpaceClicked: (Space) -> Unit,
+    viewModel: SpaceListViewModel = koinViewModel()
+) {
 
-    ) {
+    val spaceList by viewModel.spaceList.collectAsStateWithLifecycle()
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
+    // This will get called again when the screen resumes (see Fragment below)
+    LaunchedEffect(Unit) {
+        viewModel.refreshSpaces()
+    }
 
+    Box(modifier = Modifier.fillMaxSize()) {
         SpaceListScreenContent(
-            spaceList = Space.getAll().asSequence().toList(),
+            spaceList = spaceList,
             onSpaceClicked = onSpaceClicked
         )
     }
-
 }
 
 @Composable
@@ -61,7 +77,7 @@ fun SpaceListScreenContent(
                 text = stringResource(R.string.lbl_no_servers),
                 fontWeight = FontWeight.SemiBold,
                 fontSize = 18.sp,
-                color = MaterialTheme.colorScheme.surface
+                color = MaterialTheme.colorScheme.onSurface
             )
         }
     } else {
