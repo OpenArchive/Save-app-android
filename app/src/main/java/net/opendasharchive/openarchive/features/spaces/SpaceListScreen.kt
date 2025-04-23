ef67c4dd176ec59c56708b22c23d0989a8ed76d4
Elelan's Macbook Pro <evic2132@sysco.com>
2025-04-23 20:03:05 +0530

    added no servers and no folders text for server list screen and archived folder list screens

--- app/src/main/java/net/opendasharchive/openarchive/features/spaces/SpaceListScreen.kt
+++ app/src/main/java/net/opendasharchive/openarchive/features/spaces/SpaceListScreen.kt
@@ -16,8 +16,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,6 +28,7 @@ import net.opendasharchive.openarchive.core.presentation.theme.DefaultScaffoldPr
 import net.opendasharchive.openarchive.db.Space
 import net.opendasharchive.openarchive.features.main.ui.components.SpaceIcon
 import net.opendasharchive.openarchive.features.main.ui.components.dummySpaceList
+import net.opendasharchive.openarchive.R
 
 @Composable
 fun SpaceListScreen(
@@ -52,22 +55,32 @@ fun SpaceListScreenContent(
     spaceList: List<Space> = emptyList()
 ) {
 
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.spacedBy(24.dp)
-    ) {
-
-        spaceList.forEach { space ->
-
-            SpaceListItem(
-                space = space,
-                onClick = {
-                    onSpaceClicked(space)
-                }
+    if (spaceList.isEmpty()) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(
+                text = stringResource(R.string.lbl_no_servers),
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 18.sp,
+                color = MaterialTheme.colorScheme.surface
             )
+        }
+    } else {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+
+            spaceList.forEach { space ->
+
+                SpaceListItem(
+                    space = space,
+                    onClick = {
+                        onSpaceClicked(space)
+                    }
+                )
+            }
         }
     }
 }
@@ -119,6 +132,21 @@ private fun SpaceListScreenPreview() {
 
         SpaceListScreenContent(
             spaceList = dummySpaceList,
+            onSpaceClicked = {
+
+            }
+        )
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun SpaceListScreenPreview2() {
+
+    DefaultScaffoldPreview {
+
+        SpaceListScreenContent(
+            spaceList = emptyList(),
             onSpaceClicked = {
 
             }
