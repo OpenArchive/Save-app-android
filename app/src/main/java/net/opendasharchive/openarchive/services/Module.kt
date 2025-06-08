8ea594901a776ea0ccdd7150a71c5bb7ff55077b
Ryan Jennings <ryan@open-archive.org>
2025-06-07 19:31:50 -0700

    di: add missing module
    running is slow for debugging

--- app/src/main/java/net/opendasharchive/openarchive/services/Module.kt
+++ app/src/main/java/net/opendasharchive/openarchive/services/Module.kt
@@ -1,8 +1,11 @@
 package net.opendasharchive.openarchive.services
 
+import net.opendasharchive.openarchive.services.tor.torModule
 import org.koin.dsl.module
 
 internal val servicesModule = module {
 
     factory { SaveClient(get()) }
+
+    includes(torModule)
 }
