7f1ec3f787b815a0388130f3862a190a484191e9
elelanv <vickneshelelan@gmail.com>
2025-09-11 23:10:36 +0530

    added device configuration for compose responsiveness

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -199,6 +199,7 @@ dependencies {
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.ui.tooling.preview)
