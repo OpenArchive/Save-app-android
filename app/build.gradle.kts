05538d060a1fdb86c8fb92f3863f4dd6ace3eb57
elelanv <vickneshelelan@gmail.com>
2025-09-11 12:12:20 +0530

    library upgrades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ android {
         applicationId = "net.opendasharchive.openarchive"
         minSdk = 29
         targetSdk = 36
-        versionCode = 30017
+        versionCode = 30018
         versionName = "4.0.2"
         multiDexEnabled = true
         vectorDrawables.useSupportLibrary = true
